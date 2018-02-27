568f304bf00a2e16d1428434e82e469e291a049e
Ivan Damjanovic <ivan.damjanovic@number26.de>
2018-02-27 11:35:31 +0100

    Initial Commit

---
+++ StepExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StepExample
+//
+//  Created by Ivan Damjanovic on 2/27/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
